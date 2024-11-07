fe567568824a6a4bf037a87cfd8f819a18549561
Ahmed Siddique <ahmed.siddique.dev@gmail.com>
2024-11-07 17:49:30 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { View, Text, StatusBar } from 'react-native'
+import React from 'react'
+import LoginSignup from './src/components/LoginSignup'
+import { Provider } from 'react-redux'
+import { store } from './src/store/store'
+import Login from './src/components/movers/Login'
+import AppNavigator from './config/appNavigator'
+
+const App = () => {
+  return (
+    // <Provider store={store}>
+    //   <LoginSignup />
+    // </Provider>
+    <>
+<StatusBar barStyle="dark-content" backgroundColor={'white'} />
+<Provider store={store}>
+<AppNavigator/>
+</Provider>
+    </>
+  )
+}
+
+export default App
